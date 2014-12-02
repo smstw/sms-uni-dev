b8960a572cc0cd8569fc43a1def622dbe75ec291
Simon Asika <asika32764@gmail.com>
2014-12-02 16:26:02 +0800

    Add some basic functions

---
+++ src/SMS/Console/SMSConsole.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Part of devsite project. 
+ *
+ * @copyright  Copyright (C) 2014 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace SMS\Console;
+
+use Windwalker\Console\Console;
+
+/**
+ * The SMSConsole class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class SMSConsole extends Console
+{
+	/**
+	 * Property name.
+	 *
+	 * @var  string
+	 */
+	protected $name = 'SMS Console';
+
+	/**
+	 * Property description.
+	 *
+	 * @var  string
+	 */
+	protected $description = 'SMS Universal development tools';
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Part of devsite project. 
+ *
+ * @copyright  Copyright (C) 2014 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+include_once __DIR__ . '/init-joomla.php';
+
+// Your init code here
